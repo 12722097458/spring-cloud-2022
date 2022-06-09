25b75521e9eeadf6eccf68ac9b8fbf63bc9f9072
12722097458 <ayinjun1109@163.com>
2022-06-09 23:24:07 +0800

    [5.Eureka服务注册与发现][单机Eureka]服务消费方80入驻eureka Server

--- cloud-consumer-order80/src/main/java/com/ityj/springcloud/ConsumerOrder80Starter.java
+++ cloud-consumer-order80/src/main/java/com/ityj/springcloud/ConsumerOrder80Starter.java
@@ -2,8 +2,10 @@ package com.ityj.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ConsumerOrder80Starter {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerOrder80Starter.class, args);
