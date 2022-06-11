a883d371b474d981959e8407c5f942b132291d10
12722097458 <ayinjun1109@163.com>
2022-06-11 17:03:56 +0800

    [5.Eureka服务注册与发现][集群Eureka构建步骤] 服务发现@EnableDiscoveryClient获取注册在eureka上的相关信息

--- cloud-provider-payment8001/src/main/java/com/ityj/springcloud/Payment8001Starter.java
+++ cloud-provider-payment8001/src/main/java/com/ityj/springcloud/Payment8001Starter.java
@@ -3,11 +3,13 @@ package com.ityj.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @MapperScan("com.ityj.springcloud.mapper")
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class Payment8001Starter {
     public static void main(String[] args) {
         SpringApplication.run(Payment8001Starter.class, args);
--- cloud-provider-payment8001/src/main/java/com/ityj/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/ityj/springcloud/controller/PaymentController.java
@@ -2,15 +2,17 @@ package com.ityj.springcloud.controller;
 
 import com.ityj.springcloud.entity.dto.PaymentDTO;
 import com.ityj.springcloud.entity.model.CommonResult;
-import com.ityj.springcloud.entity.po.PaymentPO;
 import com.ityj.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -19,6 +21,9 @@ public class PaymentController {
 
     @Autowired
     private PaymentService paymentService;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @Value("${server.port}")
     private String serverPort;
@@ -33,6 +38,17 @@ public class PaymentController {
     public CommonResult<String> save(@RequestBody @Valid PaymentDTO paymentDTO) {
         String message = paymentService.save(paymentDTO);
         return StringUtils.hasText(message) ? CommonResult.fail(message) : CommonResult.success("ServerPort:" + serverPort);
+    }
+
+    @GetMapping("/discovery")
+    public CommonResult<DiscoveryClient> discoveryInfo() {
+        discoveryClient.getServices().forEach(log::info);
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        instances.forEach(x -> {
+            log.info(x.toString());
+            log.info(x.getUri().toString());
+        });
+        return CommonResult.success(discoveryClient);
     }
 
 }
